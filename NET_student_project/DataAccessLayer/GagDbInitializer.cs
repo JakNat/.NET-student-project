49428e508cb216c41dd1981076b676bd2c5ad64a
kuba.natonek998 <kuba.natonek998@gmail.com>
2018-05-24 07:37:14 +0200

    modified DbInitializer

--- NET_student_project/DataAccessLayer/GagDbInitializer.cs
+++ NET_student_project/DataAccessLayer/GagDbInitializer.cs
@@ -14,34 +14,243 @@ namespace NET_student_project.DataAccessLayer
         {
             var Users = new List<UserModel>
             {
-                new UserModel("user1","passwd1"),
-                new UserModel("user2","passwd2"),
-                new UserModel("user3","passwd3"),
-                new UserModel("user4","passwd4"),
-                new UserModel("user5","passwd5"),
-                new UserModel("user6","passwd6"),
-                new UserModel("user7","passwd7"),
-                new UserModel("user8","passwd8"),
+                new UserModel(){
+                    Name = "user1"
+                },
+                new UserModel(){
+                    Name = "user2"
+                },
+                new UserModel(){
+                    Name = "user3"
+                },
+                new UserModel(){
+                    Name = "user4"
+                },
+                new UserModel(){
+                    Name = "user5"
+                },
+                new UserModel(){
+                    Name = "user6"
+                },
+                new UserModel(){
+                    Name = "user7"
+                },
+                new UserModel(){
+                    Name = "user8"
+                },
+               
                 
             };
            
             var Categories = new List<CategoryModel>
             {
-                new CategoryModel("Funny"),
-                new CategoryModel("SuperHero"),
-                new CategoryModel("Wallpaper"),
-                new CategoryModel("Pic Of The Day"),
-                new CategoryModel("Animals"),
-                new CategoryModel("Ask 9GAG"),
-                new CategoryModel("Awesome"),
-                new CategoryModel("WTF"),
-                new CategoryModel("Country"),
-                new CategoryModel("Food"),
-                new CategoryModel("Gaming"),
-                new CategoryModel("GIF")
+                new CategoryModel()
+                {
+                Name = "Funny",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user1")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user6")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user7")
+                    }
+
+                }
+                },
+                new CategoryModel(){
+                Name = "Wallpaper",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user1")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user8")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user7")
+                    }
+
+                }
+
+                },
+                new CategoryModel(){
+                Name = "Pic Of The Day",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user3")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user7")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user5")
+                    }
+
+                }},
+                new CategoryModel(){
+                Name = "Animals",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user2")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user2")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user3")
+                    }
+
+                }},
+                new CategoryModel(){
+                Name = "Ask 9GAG",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user4")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user4")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user8")
+                    }
+
+                }},
+                new CategoryModel(){
+                Name = "Awesome",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user2")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user4")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user8")
+                    }
+
+                }},
+                new CategoryModel(){
+                Name = "WTF",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user1")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user6")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user7")
+                    }
+
+                }},
+                new CategoryModel(){
+                Name = "Country",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user1")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user4")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user3")
+                    }
+
+                }},
+                new CategoryModel(){
+                Name = "Food",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user5")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user6")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user7")
+                    }
+
+                }},
+                new CategoryModel(){
+                Name = "Gaming",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user8")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user2")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user4")
+                    }
+
+                }},
+                new CategoryModel(){
+                Name = "Gif",
+                Memes = new List<MemeModel>
+                {
+                    new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user4")
+                    },
+                     new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user5")
+                    },
+                      new MemeModel
+                    {
+                        User = Users.First(u => u.Name == "user2")
+                    }
+
+                }}
+               
             };
-            var Memes = new List<MemeModel>
-            {
+           // var Memes = new List<MemeModel>
+            //{
+            /*
 
                 new MemeModel(Users.First(p => p.Name == "user1"),Categories.First(p => p.Name == "GIF")),
                 new MemeModel(Users.First(p => p.Name == "user1"),Categories.First(p => p.Name == "Gaming")),
@@ -62,14 +271,16 @@ namespace NET_student_project.DataAccessLayer
                 new MemeModel(Users.First(p => p.Name == "user7"),Categories.First(p => p.Name == "Food")),
                 new MemeModel(Users.First(p => p.Name == "user7"),Categories.First(p => p.Name == "Pic Of The Day")),
                 new MemeModel(Users.First(p => p.Name == "user8"),Categories.First(p => p.Name == "Gaming")),
-             
+             */
+          //   new MemeModel(new UserModel("user","pswd"),new CategoryModel("gaming")),
+            // new MemeModel(new UserModel("use","psw"),new CategoryModel("funny")
                 
              
 
-            };
-
+         //   )};
+            
             context.Users.AddRange(Users);
-            context.Memes.AddRange(Memes);
+        //    context.Memes.AddRange(Memes);
             context.Categories.AddRange(Categories);    
             base.Seed(context);
         }
