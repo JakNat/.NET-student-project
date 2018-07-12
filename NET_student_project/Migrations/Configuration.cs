89fc00af16eef479430d1b78ac587e0b0c02e77c
kuba.natonek998 <kuba.natonek998@gmail.com>
2018-07-12 17:18:14 +0200

    Fixed bugs with identity

--- NET_student_project/Migrations/Configuration.cs
+++ NET_student_project/Migrations/Configuration.cs
@@ -15,7 +15,7 @@ namespace NET_student_project.Migrations
         protected override void Seed(NET_student_project.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
-
+         
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
         }
