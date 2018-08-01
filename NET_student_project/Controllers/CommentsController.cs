8bb6e2193771384147e7cb124328607458c96e36
kuba.natonek998 <kuba.natonek998@gmail.com>
2018-08-01 09:35:39 +0200

    updated comment system

--- NET_student_project/Controllers/CommentsController.cs
+++ NET_student_project/Controllers/CommentsController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using NET_student_project.DataAccessLayer;
+using NET_student_project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +12,65 @@ namespace NET_student_project.Controllers
 {
     public class CommentsController : Controller
     {
-        // GET: Comments
-        public ActionResult Index()
+        private readonly GagDbContext _gagDb = new GagDbContext();
+
+        public ActionResult AddComment()
         {
-            return View();
+            return PartialView();
         }
+        [Authorize]
+        [ChildActionOnly]
+        [HttpPost]
+        public ActionResult AddComment(int id, string message)
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var name = identity.GetUserName();
+            CommentModel comm = new CommentModel
+            {
+                User = _gagDb.Users.First(x => x.Name == name),
+                Text = message,
+                Meme = _gagDb.Memes.First(x => x.Id == id)
+            };
+            _gagDb.Comments.Add(comm);
+            _gagDb.SaveChanges();
+            return PartialView();
+        }
+
+        public ActionResult _AddSubComment(int commentid)
+        {
+            ViewBag.id = commentid;
+            return PartialView();
+        }
+        [Authorize]
+        [ChildActionOnly]
+        [HttpPost]
+        public ActionResult _AddSubComment(int commentid, string subMessage)
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var name = identity.GetUserName();
+            CommentModel comm = new CommentModel
+            {
+                User = _gagDb.Users.First(x => x.Name == name),
+                Text = subMessage,
+               
+            };
+            try
+            {
+                _gagDb.Comments.First(c => c.Id == commentid).SubComments.Add(comm);
+            }
+            catch (Exception)
+            {
+                _gagDb.Comments.First(c => c.Id == commentid).SubComments = new List<CommentModel>();
+                _gagDb.Comments.First(c => c.Id == commentid).SubComments.Add(comm);
+            }
+           
+            _gagDb.SaveChanges();
+
+            return PartialView(
+
+            );
+        }
+
+
     }
 }
