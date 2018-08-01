1d7f46f22f89eff99cf854d7d351d552a2498021
kuba.natonek998 <kuba.natonek998@gmail.com>
2018-08-01 09:37:11 +0200

    Added UserRepository

---
+++ NET_student_project/DataAccessLayer/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NET_student_project.DataAccessLayer
+{
+    public class UserRepository
+    {
+        private GagDbContext _gag = new GagDbContext();
+        
+       public List<string> GetAvatarsPath()
+        {
+            var list = new List<string>();
+            foreach(var user in _gag.Users)
+            {
+                var path = user.ImagePath;
+                if (!list.Contains(path)) 
+                {
+                    list.Add(path);
+                }
+
+            }
+            return list;
+        }
+
+    }
+}
